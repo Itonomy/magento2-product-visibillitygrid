75b0707b94d1c025ddda8309f3b661ac325fb493
Daniel Azulay <d.r.azulay@gmail.com>
2018-08-15 13:27:50 +0200

    Update ProductIndexer.php

--- Model/ProductIndexer.php
+++ Model/ProductIndexer.php
@@ -1,28 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: benvansteenbergen
- * Date: 07/08/2018
- * Time: 11:37
- */
 
 namespace Itonomy\ProductVisibilityGrid\Model;
 
+use \Magento\Indexer\Model\IndexerFactory;
 
+/**
+ * Product indexer
+ *
+ * @category  Class
+ * @author    Ben van Steenbergen <ben.vansteenbergen@itonomy.nl>
+ * @author    Daniel R. Azulay <daniel.azulay@itonomy.nl>
+ */
 class ProductIndexer
 {
-    protected $indexerFactory;
-
-    public function __construct(
-        \Magento\Indexer\Model\IndexerFactory $indexerFactory
-    )
+    /** @var IndexerFactory $indexerFactory */
+    protected $indexerFactory = null;
+    
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
+    public function __construct(IndexerFactory $indexerFactory)
     {
         $this->indexerFactory = $indexerFactory;
     }
-
-    public function reindexList(array $productIds) {
-
-        $indexerIds = array(
+    
+    /**
+     * Gets indexer codes.
+     *
+     * @return void
+     */
+    public function getIndexerCodes()
+    {
+        return [
             'catalog_product_flat',
             'catalog_category_product',
             'catalog_product_category',
@@ -31,14 +42,36 @@ class ProductIndexer
             'cataloginventory_stock',
             //'catalogrule_product',
             'catalogsearch_fulltext',
-        );
-        foreach ($indexerIds as $indexerId) {
-            $indexer = $this->indexerFactory->create();
-            $indexer->load($indexerId);
-            $indexer->reindexList(array_unique($productIds));
+        ];
+    }
+    
+    /**
+     * Reindexes product IDs.
+     *
+     * @param int[]|string[] $productIds Product IDs
+     *
+     * @return bool
+     */
+    public function reindexList(array $productIds)
+    {
+        $indexerCodes = $this->getIndexerCodes();
+        foreach ($indexerCodes as $indexerCode) {
+            $this->getIndexerByCode($indexerCode)->reindexList(
+                \array_unique($productIds)
+            );
         }
-
         return true;
     }
-
+             
+    /**
+     * Gets indexer instance by indexer code.
+     * 
+     * @param string $indexerCode Indexer code
+     *
+     * @return void
+     */
+    protected function getIndexerByCode($indexerCode)
+    {      
+        return $this->indexerFactory->create()->load($indexerCode);
+    }
 }
