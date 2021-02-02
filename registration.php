129df1da3f69e6c18d51b809c8a88e9cd672c755
Ben van Steenbergen <ben.vansteenbergen@itonomy.nl>
2021-02-02 12:17:26 +0100

    Update module name issue

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Itonomy_ProductVisibilityGrid__',
+    'Itonomy_ProductVisibilityGrid',
     __DIR__
 );
